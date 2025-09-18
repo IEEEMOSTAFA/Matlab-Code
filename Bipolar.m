7fb18408f33b49125b83277b08e63ff83ccad63a
Md Al Mostafa <skmostafa8888@gmail.com>
2025-09-18 18:28:33 +0600

    Bipolar code added

---
+++ Bipolar.m
@@ -0,0 +1,23 @@
+m = [1 0 0 1 0 1 0 1 1 1]; 
+n = length(m);
+x = [];
+y = [];
+a=1;
+for i=1:n
+    x=[x i-1 i];
+    if(m(i)== 1)
+        y=[y a a];
+        a=a*(-1);
+    else
+        y=[y 0 0];
+    end
+end
+
+plot(x,y),axis([0,n,-2,2]);
+title('Bipolar AMI');
+xlabel('Time');
+ylabel('Amplitude');
+for i = 1:n
+    text(i-0.5, 1.2, num2str(m(i)));
+end
+grid on
